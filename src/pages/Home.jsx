eb3b515006be5747c2bef57fb604baeb822a3499
AkshayKulkarni19 <kulkarni.19@gmail.com>
2025-04-13 12:27:03 +0530

    categories added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainBanner from "../components/MainBanner";
+import Categories from "../components/Categories";
+
+const Home = () => {
+  return (
+    <div className="mgt-10">
+      <MainBanner />
+      <Categories />
+    </div>
+  );
+};
+
+export default Home;
